ca74518950376e6ea8d4d8fb3937893fae4af486
pank3004 <pankajkumawatew3004@gmail.com>
2025-04-07 14:43:30 +0530

    complete all modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "Wafer"
+MONGO_COLLECTION_NAME = "Wafer_Fault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://pankajkumawat04:PankajKumawat123@cluster0.8yeeask.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+ARTIFACT_FOLDER =  "artifacts"
