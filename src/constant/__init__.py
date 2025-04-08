f33b90f4ce14fe138908632af0d0f86e0e880e5a
pank3004 <pankajkumawatew3004@gmail.com>
2025-04-08 11:05:07 +0530

    test3

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -21,6 +21,8 @@ AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
 REGION_NAME = "us-east-1"
 
+ECR_REPO='010928200946.dkr.ecr.us-east-1.amazonaws.com/waferproj'
+
 
 
 
